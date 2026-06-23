0d479a89da8195344e6107ba77fadf4221b0ac88
aashi34 <aashi.25559@stu.upes.ac.in>
2026-06-23 20:15:45 +0530

    5th

---
+++ passwordgenerator.py
@@ -0,0 +1,9 @@
+import random
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+
+length = int(input("Enter the length of the password: "))
+password = ""
+
+for i in range(length):
+    password+= random.choice(characters)
+print(password)
